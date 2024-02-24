48c1af8f878d1e4547f1264fc531d42c43b53315
shakibuljoy <shakibulalamj@gmail.com>
2024-02-24 22:46:59 +0600

    added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css'
 import {inter} from '@/app/ui/fonts'
 import { Metadata } from 'next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: {
